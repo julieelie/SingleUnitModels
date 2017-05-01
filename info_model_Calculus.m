2f5b1b2dee805aca480ccb2b6255e98367453fe4
Julie Elie <julie.elie@gmail.com>
2017-04-30 22:42:50 -0700

    Simplification of the code and nice comments

--- info_model_Calculus.m
+++ info_model_Calculus.m
@@ -1,212 +1,137 @@
-function [I,P_YgivenS_all1,P_YgivenS_all2,H_y, H_ymu]=info_model_Calculus(mu_in)
+function [I,P_YgivenS_all,H_y, H_ymu]=info_model_Calculus(Mu_in)
 %% Calculates the information obtain from N Poisson with different means.
 
-% This functions is used to calculate the mutual information between n categorical stimuli and
-% a poisson response defined by its n means.  The maximum value is log2(n).  The minum value is
-% zero and will be obtained when the n means are identical.
-% This function assumes that the mean is between 0 and 50.
+% This functions is used to calculate the mutual information between n
+% events (stimuli, categories of stimuli, motor output...) and
+% a poisson response defined by its n means. The maximum value is log2(n).
+% The minum value is zero and will be obtained when the n means are identical.
+% This function assumes a Poisson noise for the neural response with a mean
+% between 0 and 50.
 % Write your own routine for values higher than 50 using a the Normal approximation
 % for the Poisson distribution.
 
+
 % Requires:
-% mu_in : vector 1xn that specifies the mean (in count/bin i.e. no units here) of the n Poisson distributions.
-
-% Returns:
-
+% Mu_in         vector 1xn that specifies the mean (in count/bin i.e. no
+%               units here) of the n Poisson distributions.
 
 
-% Note that there is a trade off in the information value calculation
-% between the values that mu can get and how widely the entropy of the model
-% can be estimated over the possible values of y. if ymax is fixed to 170
-% then mu has to be lower than 65 for now.
-% mu_in are the predictions from the model for every single stim and every
-% single trial
-% ymax is the maximum number of spike you can observed in your dataset. We
-% can indeed reasonably uperbound the calculation of entropy on all
-% possible y but the maximum nuber of spikes you can observed given the
-% size of your observation window (like 20ms -> 20 spikes) instead of
-% summing to + infinity.
-% y is optional and is the observed data if you want to estimate the
-% entropy of the neural response as a histogram (Bialeck kind of
-% estimation)
+% Returns: 
+% I             the mutual information in bits between n events (stimuli,
+%               categories of stimuli, motor output...) and a poisson
+%               response defined by its means mu_in.
 
-% mu_max is the maximum value expected for the model prediction, anything
-% above it does not give any information as the model of the response is
-% obviously wrong. For those values of mu>mu_max, the probability is set to
-% zero
+% P_YgivenS_all a matrix ym x n where each column gives the conditional
+%               probability distribution of the response given the stimulus
+%               i.e. the input mean mu_in(n). ym corresponds to the number 
+%               of values of y (0:Ymax) that are used to estimate entropies.
 
+% H_y           a scalar that specifies the entropy of the response 
+
+% H_y_mu        a scalar that specifies the entropy of the response given
+%               the stimulus i.e., the input means Mu_in.
+
+%% Sanitary checks and parameters setting
+% Check for max value of Mu_in to be below upper bound for a Poisson
+% distribution assumption.
+MAXMEANVALUE = 50;
+if ~sum(Mu_in <= MAXMEANVALUE)
+    ERRMSG= 'This function assumes Poisson distribution means inferior or equal to %d spikes per bin. Write your own routine for values higher than 50 using a the Normal approximation for the Poisson distribution.';
+    error(ERRMSG, MAXMEANVALUE)
+end
+
+
+% Set a lower bound for Mu_in
+% We need to attend the case of mu=0 which ends up in abberant calculations
+% because log(0) = -Inf.
+% The natural increment for mu is 1 so let's have log(mu)
+% almost linear around 0 (around mu=1) and fix muLims=1/20.
+MuLims=1/20;
 % Here we know the natural scale for mu: the neuron can spike between
 %0 and 1 time per ms so depending on the window size Win (ms) the range
 % of values is 0 to Win spikes. Make the lower limit for log(mu) as
 % - the max limit by replacing zero values in mu by muLims=1/Win.
-% Other idea: the natural increment for mu is 1 so let's have log(mu)
-% almost linear around 0 (around mu=1) and fix muLims=1/20.
+% Other idea: 
 % In glmfit and here if no scale is provided, other choice because they don't know the
 % natural scale of mu. Their choice keeps mu^4 from underflowing.No upper limit.
-
-% Flag to force normalization of prob distribution as it is calculated from
-% 0 to ymax only.  Should not be set to zero unless you want to estimate
-% the size of the effect.
-
-SameDataScaleEntropy=1; 
-
-% if nargin<2
-%     mu_max = 100*median(mu_in);
-% end
-% 
-% if any(mu_in>(10*mu_max))
-%     OkPredict = find(mu_in<(10*mu_max));% These are the mu that give information the others are abberant predictions that are non informative
-%     mu_in_checked = mu_in(OkPredict); 
-%     fprintf('%d/%d mu were suppressed from the entry as those are abberant\n',length(mu_in) - length(mu_in_checked),length(mu_in));
-% else
-    %I'm not checking anymore the mean spike rate of poisson distribution for the different stims. I assume that these predictions should be fine (11/29/2016)
-     mu_in_checked = mu_in;
-     OkPredict = 1:length(mu_in);
-% end
-
-MAXMEANVALUE = 50;
-
-if nargin<4
-%     dataClass = superiorfloat(mu,y);
-%     muLims = realmin(dataClass).^.25; %Choice in glmfit
-    Scale=20;%Best value for our data
-    muLims = 1/Scale;
-else
-    %muLims = 1/2;%make the log function linear just around 1 (log(1)=0)
-    muLims = 1/Scale;%exp(-log(Win))
-end
-
-if nargin<3
-    Yhist_Flag = 0;
-else
-    Yhist_Flag=1;
-end
-
-if nargin<2
-    ymax = 20; % For julie's data spike count are within 10ms windows!!
-    %ymax = ceil(max(mu_in_checked*10));
-end
-if ymax>170
-    ymax=170; % factorial is reaching Inf if ymax>170
-end
-
-% Check for max value of mu_in to be below 50.
-
-% Y max is three standard deviations above the maximum the variance
-ymax = 3.0*sqrt(max(mu_in)) + max(mu_in);
+% dataClass = superiorfloat(mu,y);
+% muLims = realmin(dataClass).^.25; %Choice in glmfit
 
 
-    
-N_pres = length(mu_in_checked);
-y_world = 0:ymax;
-[mu_unique, ~, I_mu_unique] = unique(mu_in_checked); % mu most likely has repetitions du to the trial presentations 
-Nb_mu = length(mu_unique);
+% Y_world defines the range of values that are used to calculate the entropies
+% Ymax corresponds to the maximum number of spikes you can observed per bin
+% given the iput mean. We can indeed reasonably limit the calculation
+% of entropy from 0 up to the maximum number of spikes expected instead of
+% summing to + infinity.
+% Ymax is three standard deviations above the maximum variance (note that
+% the mean equals the variance for Poisson distributions).
+Ymax = 3.0*sqrt(max(Mu_in)) + max(Mu_in);
+Y_world = 0:Ymax;
+
+% To save time, calculations are only done for unique values of Mu_in    
+[Mu_unique, ~, I_mu_unique] = unique(Mu_in); 
+Nb_Unq_mu = length(Mu_unique);
 
 
-%% Calculate the conditional entropy (H(y/mu))
+%% Calculate the conditional entropy H(y/mu) assuming a Poisson distribution
+% Initialize output variables
+N_mu = length(Mu_in);
+P_YgivenS_all = nan(length(Y_world),N_mu);
+P_YgivenS_all_rescaled = nan(length(Y_world),N_mu);
 
-% To save time this is only done for unique values of mu.
-H_ymu_unique = nan(size(mu_unique)); 
-Fac_y = factorial(y_world);   % Saved to save computational time.
+% To save time calculation of conditional entropy is only done for unique values of mu.
+H_ymu_unique = nan(size(Mu_unique));
 
-P_YgivenS_all1_local = nan(length(y_world),N_pres);
-P_YgivenS_all1_rescaled = nan(length(y_world),N_pres);
+% Y factorial
+Fac_y = factorial(Y_world);   % Saved to save computational time.
 
-for mm=1:Nb_mu
+% Now loop through unique mu and calculate the conditional entropy for each
+% of them
+for mm=1:Nb_Unq_mu
     
     % Local value of mean
-    mu = mu_unique(mm);
+    Mu = Mu_unique(mm);
     
     % Calculate the log of the probability for numerical reasons.
-    Log_P = nan(size(y_world));   % Initialize
+    Log_P = nan(size(Y_world));   % Initialize
     
     % Deal with cases where mu is equal to zero and/or is below muLims
-    if mu == 0
+    if Mu == 0
         Log_P(1) = 1;  % The value for y=0
-        mu = muLims;
-        Log_P(2:end)= y_world(2:end).*log2(mu) - log2(Fac_y(2:end)) - mu/log(2);
-    elseif mu < muLims
-        mu = muLims;
-        Log_P = y_world.*log2(mu) - log2(Fac_y) - mu/log(2);
+        Mu = MuLims;
+        Log_P(2:end)= Y_world(2:end).*log2(Mu) - log2(Fac_y(2:end)) - Mu/log(2);
+    elseif Mu < MuLims
+        Mu = MuLims;
+        Log_P = Y_world.*log2(Mu) - log2(Fac_y) - Mu/log(2);
     else
-        Log_P = y_world.*log2(mu) - log2(Fac_y) - mu/log(2);
+        Log_P = Y_world.*log2(Mu) - log2(Fac_y) - Mu/log(2);
     end
     
     % From logP to P
     P = 2.^(Log_P);
-    P_YgivenS_all1_local(:,find(I_mu_unique==mm))=repmat(P',1,length(find(I_mu_unique==mm)));
+    P_YgivenS_all(:,(I_mu_unique==mm))=repmat(P',1,sum(I_mu_unique==mm));
    
     % Check that sum(P) sum to 1 and rescale if not to have a more
     % realistic distribution.
     P = P ./sum(P);
-    P_YgivenS_all1_rescaled(:,find(I_mu_unique==mm))=repmat(P',1,length(find(I_mu_unique==mm)));
-    H_ymu_unique(mm) = sum(-P.*log2(P + (P==0)));% conditional entropy value for each unique mu (each stim) across all y of the world
+    P_YgivenS_all_rescaled(:,(I_mu_unique==mm))=repmat(P',1,sum(I_mu_unique==mm));
+    % conditional entropy value for each unique mu (each stim) across all y of the world
+    H_ymu_unique(mm) = sum(-P.*log2(P + (P==0))); % Note that adding P==0 in the logarithmic expression ensures that P*log2(P) = 0 when P=0.
 end
 
 % Given that all stimuli have equal probability: H(y|s) = expectation(H(y|si)) = sum(p(si)*H(y|si)) = sum(H(y|si))/(number of stims)
+H_ymu = sum(H_ymu_unique(I_mu_unique))/N_mu;
 
-H_ymu = sum(H_ymu_unique(I_mu_unique))/N_pres;
 
-%% Calculate the entropy of y based on Poisson distributions
-% This calculation is not using the data as scaled per stim as used for the
-% calculation of the exact entropy
-if ~SameDataScaleEntropy
-    P_YgivenS_all2_local = nan(length(y_world),N_pres);
-    for ii=1:(ymax +1)
-        yy = y_world(ii);
-        y_local = repmat(yy,size(mu_in_checked));
-        Fac_y = repmat(factorial(yy),size(mu_in_checked));
-        if any(mu_in_checked < muLims) % This is just to be consistent with the rules regarding mu that were applied higher
-            mu_temp = max(mu_in_checked,muLims);
-            P_YgivenS_all2_local(ii,:) = (mu_temp.^y_local) .* exp(-mu_temp) ./ Fac_y;
-        else
-            P_YgivenS_all2_local(ii,:) = (mu_in_checked.^y_local) .* exp(-mu_in_checked) ./ Fac_y;
-        end
-    end
-    
-    P_y_i = sum(P_YgivenS_all2_local,2)./N_pres;
-    % Check that sum(P) sum to 1 and rescale if not to have a more
-    % realistic distribution.
-    if sum(P_y_i)~=1
-        P_y_i = P_y_i ./sum(P_y_i);
-        fprintf('rescaled the distribution of y\n');
-    end
-    
-else
-    % Calculation using the same data as for the conditional entropy
-    P_y_i = sum(P_YgivenS_all1_rescaled,2)./N_pres;
-end
-% Calculate log probability
+%% Calculate the entropy of the response y based on Poisson distributions
+% Probability of responses (marginal)
+P_y_i = sum(P_YgivenS_all_rescaled,2)./N_mu;
+
+% Entropy of the response
 H_y = sum(-P_y_i.*log2(P_y_i + (P_y_i==0)));% conditional entropy value for each unique y of the world across all mu of the model
 
-%% Calculate the model information
+%% Calculate mutual information
 I = (H_y - H_ymu);
-
-%% OUtput matrices of conditional probabilities for all mu in, even the one that wee aberrant by setting values to 0
-P_YgivenS_all1 = zeros(length(y_world),length(mu_in));
-P_YgivenS_all1(:,OkPredict) = P_YgivenS_all1_local;
-
-if ~SameDataScaleEntropy
-    P_YgivenS_all2 = zeros(length(y_world),length(mu_in));
-    P_YgivenS_all2(:,OkPredict) = P_YgivenS_all2_local;
-else
-    P_YgivenS_all2=[];
-end
-
-%% Calculate H(y) using the actual dataset
-if Yhist_Flag
-    P_y = nan(ymax +1,1);
-    for ii = 1:(ymax +1)
-        yy = y_world(ii);
-        P_y(ii) = sum(y==yy)./length(y);
-    end
-    H_y_histo = sum(-P_y.*log(P_y + (P_y==0)));
-    I_histo = H_y_histo - H_ymu;
-    I = [I I_histo];
-end
-
-    
-
 
 end
 
