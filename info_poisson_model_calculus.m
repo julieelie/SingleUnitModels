64231d2f56635691fbe6ca901635e5496b2c424c
Julie Elie <julie.elie@gmail.com>
2017-05-09 12:54:28 -0700

    nicer comment of the code

--- info_poisson_model_calculus.m
+++ info_poisson_model_calculus.m
@@ -28,7 +28,7 @@ function [I,P_YgivenS,H_Y, H_YgivenS]=info_poisson_model_calculus(Mu_in, varargi
 %               spike rate should be 1/2 * 1/NTrials. By default the value
 %               is set to 1/20, so works well for 10 trials datasets.
 
-%    'Ymax'     Y_world defines the range of values that are used to
+%    'Ymax'     Y_range defines the range of values that are used to
 %               calculate the entropies. Ymax corresponds to the maximum
 %               number of spikes you can observed per bin given the input
 %               mean. We can indeed reasonably limit the calculation
@@ -82,23 +82,23 @@ end
 
 
 % Set the range of values that are used to calculate the entropies.
-Y_world = 0:Ymax;
+Y_range = 0:Ymax;
 
 % To save time, calculations are only done for unique values of Mu_in    
-[Mu_unique, ~, I_mu_unique] = unique(Mu_in);
+[Mu_unique, ~, Ind_mu_unique] = unique(Mu_in);
 
 
 %% Calculate the conditional entropy H(y/mu) assuming a Poisson distribution
 % Initialize output variables
 N_mu = length(Mu_in);
-P_YgivenS = nan(length(Y_world),N_mu);
-P_YgivenS_rescaled = nan(length(Y_world),N_mu);
+P_YgivenS = nan(length(Y_range),N_mu);
+P_YgivenS_rescaled = nan(length(Y_range),N_mu);
 
 % To save time calculation of conditional entropy is only done for unique values of mu.
 H_YgivenS_unique = nan(size(Mu_unique));
 
 % Y factorial
-Fac_y = factorial(Y_world);   % Saved to save computational time.
+Fac_y = factorial(Y_range);   % Saved to save computational time.
 
 % Now loop through unique mu and calculate the conditional entropy for each
 % of them
@@ -108,39 +108,38 @@ for mm=1:length(Mu_unique)
     Mu = Mu_unique(mm);
     
     % Calculate the log of the probability for numerical reasons.
-    % Deal with cases where mu is below muLims
+    % Deal with cases where Mu is below MuLims
     if Mu < MuLims
         Mu = MuLims;
     end
-    Log_P = Y_world.*log2(Mu) - log2(Fac_y) - Mu/log(2);
+    Log_P = Y_range.*log2(Mu) - log2(Fac_y) - Mu/log(2);
     
     % From logP to P
     P = 2.^(Log_P);
-    P_YgivenS(:,(I_mu_unique==mm))=repmat(P',1,sum(I_mu_unique==mm));
+    % Fill in P_YgivenS for every column that shares the same mean (Mu_in)
+    P_YgivenS(:,(Ind_mu_unique==mm))=repmat(P',1,sum(Ind_mu_unique==mm));
    
     % Check that sum(P) sum to 1 and rescale if not to have a more
     % realistic distribution.
     P = P ./sum(P);
-    P_YgivenS_rescaled(:,(I_mu_unique==mm))=repmat(P',1,sum(I_mu_unique==mm));
+    % Fill in P_YgivenS_rescaled for every column that shares the same mean (Mu_in)
+    P_YgivenS_rescaled(:,(Ind_mu_unique==mm))=repmat(P',1,sum(Ind_mu_unique==mm));
     % conditional entropy value for each unique mu (each stim) across all y of the world
     H_YgivenS_unique(mm) = sum(-P.*log2(P + (P==0))); % Note that adding P==0 in the logarithmic expression ensures that P*log2(P) = 0 when P=0.
 end
 
 % Given that all stimuli have equal probability: H(y|s) = expectation(H(y|si)) = sum(p(si)*H(y|si)) = sum(H(y|si))/(number of stims)
-H_YgivenS = sum(H_YgivenS_unique(I_mu_unique))/N_mu;
+H_YgivenS = sum(H_YgivenS_unique(Ind_mu_unique))/N_mu;
 
 
 %% Calculate the entropy of the response y based on Poisson distributions
 % Probability of responses (marginal)
-P_y_i = sum(P_YgivenS_rescaled,2)./N_mu;
+P_Y = sum(P_YgivenS_rescaled,2)./N_mu;
 
 % Entropy of the response
-H_Y = sum(-P_y_i.*log2(P_y_i + (P_y_i==0)));% conditional entropy value for each unique y of the world across all mu of the model
+H_Y = sum(-P_Y.*log2(P_Y + (P_Y==0)));% entropy of the response = entropy of the marginals = entropy of the average response probabilities over all Mu_in
 
 %% Calculate mutual information
 I = (H_Y - H_YgivenS);
 
 end
-
-
-
