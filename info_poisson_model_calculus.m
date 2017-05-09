15ef8d6127f1469e8bde331a972cb8723ef2ec56
Julie Elie <julie.elie@gmail.com>
2017-05-09 12:29:12 -0700

    nicer outfit of the code

--- info_poisson_model_calculus.m
+++ info_poisson_model_calculus.m
@@ -1,4 +1,4 @@
-function [I,P_YgivenS_all,H_y, H_ymu]=info_model_Calculus(Mu_in, varargin)
+function [I,P_YgivenS,H_Y, H_YgivenS]=info_poisson_model_calculus(Mu_in, varargin)
 %% Calculates the information obtain from N Poisson with different means.
 
 % This functions is used to calculate the mutual information between n
@@ -44,14 +44,14 @@ function [I,P_YgivenS_all,H_y, H_ymu]=info_model_Calculus(Mu_in, varargin)
 %               categories of stimuli, motor output...) and a poisson
 %               response defined by its means mu_in.
 
-% P_YgivenS_all a matrix ym x n where each column gives the conditional
+% P_YgivenS     a matrix ym x n where each column gives the conditional
 %               probability distribution of the response given the stimulus
 %               i.e. the input mean mu_in(n). ym corresponds to the number 
 %               of values of y (0:Ymax) that are used to estimate entropies.
 
-% H_y           a scalar that specifies the entropy of the response 
+% H_Y           a scalar that specifies the entropy of the response 
 
-% H_y_mu        a scalar that specifies the entropy of the response given
+% H_YgivenS        a scalar that specifies the entropy of the response given
 %               the stimulus i.e., the input means Mu_in.
 
 %% Sanitary checks and parameters setting
@@ -65,15 +65,15 @@ end
 
 
 % Sorting input arguments
-pnames = {'MuLims','Ymax'};
+Pnames = {'MuLims','Ymax'};
 
 % Calculating default values of input arguments
 MuLims=1/20;
 Ymax_def = fix(3.0*sqrt(max(Mu_in)) + max(Mu_in));
 
 % Get input arguments
-dflts  = {MuLims Ymax_def};
-[MuLims, Ymax] = internal.stats.parseArgs(pnames,dflts,varargin{:});
+Dflts  = {MuLims Ymax_def};
+[MuLims, Ymax] = internal.stats.parseArgs(Pnames,Dflts,varargin{:});
 if fix(Ymax)<Ymax_def
     ERRMSG= 'The range of neural response values that are used to calculate mutual information is too small.\nThe maximum value investigated is %d when it should be at least %d given the maximum mean entered as input to the function.\n';
     error(ERRMSG, Ymax, Ymax_def)
@@ -85,25 +85,24 @@ end
 Y_world = 0:Ymax;
 
 % To save time, calculations are only done for unique values of Mu_in    
-[Mu_unique, ~, I_mu_unique] = unique(Mu_in); 
-Nb_Unq_mu = length(Mu_unique);
+[Mu_unique, ~, I_mu_unique] = unique(Mu_in);
 
 
 %% Calculate the conditional entropy H(y/mu) assuming a Poisson distribution
 % Initialize output variables
 N_mu = length(Mu_in);
-P_YgivenS_all = nan(length(Y_world),N_mu);
-P_YgivenS_all_rescaled = nan(length(Y_world),N_mu);
+P_YgivenS = nan(length(Y_world),N_mu);
+P_YgivenS_rescaled = nan(length(Y_world),N_mu);
 
 % To save time calculation of conditional entropy is only done for unique values of mu.
-H_ymu_unique = nan(size(Mu_unique));
+H_YgivenS_unique = nan(size(Mu_unique));
 
 % Y factorial
 Fac_y = factorial(Y_world);   % Saved to save computational time.
 
 % Now loop through unique mu and calculate the conditional entropy for each
 % of them
-for mm=1:Nb_Unq_mu
+for mm=1:length(Mu_unique)
     
     % Local value of mean
     Mu = Mu_unique(mm);
@@ -117,29 +116,29 @@ for mm=1:Nb_Unq_mu
     
     % From logP to P
     P = 2.^(Log_P);
-    P_YgivenS_all(:,(I_mu_unique==mm))=repmat(P',1,sum(I_mu_unique==mm));
+    P_YgivenS(:,(I_mu_unique==mm))=repmat(P',1,sum(I_mu_unique==mm));
    
     % Check that sum(P) sum to 1 and rescale if not to have a more
     % realistic distribution.
     P = P ./sum(P);
-    P_YgivenS_all_rescaled(:,(I_mu_unique==mm))=repmat(P',1,sum(I_mu_unique==mm));
+    P_YgivenS_rescaled(:,(I_mu_unique==mm))=repmat(P',1,sum(I_mu_unique==mm));
     % conditional entropy value for each unique mu (each stim) across all y of the world
-    H_ymu_unique(mm) = sum(-P.*log2(P + (P==0))); % Note that adding P==0 in the logarithmic expression ensures that P*log2(P) = 0 when P=0.
+    H_YgivenS_unique(mm) = sum(-P.*log2(P + (P==0))); % Note that adding P==0 in the logarithmic expression ensures that P*log2(P) = 0 when P=0.
 end
 
 % Given that all stimuli have equal probability: H(y|s) = expectation(H(y|si)) = sum(p(si)*H(y|si)) = sum(H(y|si))/(number of stims)
-H_ymu = sum(H_ymu_unique(I_mu_unique))/N_mu;
+H_YgivenS = sum(H_YgivenS_unique(I_mu_unique))/N_mu;
 
 
 %% Calculate the entropy of the response y based on Poisson distributions
 % Probability of responses (marginal)
-P_y_i = sum(P_YgivenS_all_rescaled,2)./N_mu;
+P_y_i = sum(P_YgivenS_rescaled,2)./N_mu;
 
 % Entropy of the response
-H_y = sum(-P_y_i.*log2(P_y_i + (P_y_i==0)));% conditional entropy value for each unique y of the world across all mu of the model
+H_Y = sum(-P_y_i.*log2(P_y_i + (P_y_i==0)));% conditional entropy value for each unique y of the world across all mu of the model
 
 %% Calculate mutual information
-I = (H_y - H_ymu);
+I = (H_Y - H_YgivenS);
 
 end
 
